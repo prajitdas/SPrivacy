843c0b5cfe992ef873c9f9afc6e6bdbd5e22a659
Prajit Kumar Das <prajitdas@gmail.com>
2014-07-09 14:35:23 -0700

    added URIs and provider info

--- src/com/prajitdas/sprivacy/contentprovider/PrivacyAwareContentContracts.java
+++ src/com/prajitdas/sprivacy/contentprovider/PrivacyAwareContentContracts.java
@@ -1,6 +1,24 @@
 package com.prajitdas.sprivacy.contentprovider;
 
+import android.net.Uri;
+
 public class PrivacyAwareContentContracts {
-    public static final String MEDIA_AUTHORITY = "media";
-    private static final String MEDIA_CONTENT_AUTHORITY_SLASH = "content://" + MEDIA_AUTHORITY + "/";
+    private static final String FAKE_IMAGE_PROVIDER_NAME = "com.prajitdas.sprivacy.contentprovider.util.fake";
+    private static final String FAKE_IMAGE_URL = "content://" + FAKE_IMAGE_PROVIDER_NAME;
+    private static final Uri FAKE_IMAGE_CONTENT_URI = Uri.parse(FAKE_IMAGE_URL);
+
+    private static final String REAL_IMAGE_PROVIDER_NAME = "com.prajitdas.sprivacy.contentprovider.util.real";
+    private static final String REAL_IMAGE_URL = "content://" + REAL_IMAGE_PROVIDER_NAME;
+    private static final Uri REAL_IMAGE_CONTENT_URI = Uri.parse(REAL_IMAGE_URL);
+
+    public static Uri getImageContentUri() {
+		if(isThisAllowed())
+			return REAL_IMAGE_CONTENT_URI;
+		return FAKE_IMAGE_CONTENT_URI;
+	}
+	
+	private static boolean isThisAllowed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
