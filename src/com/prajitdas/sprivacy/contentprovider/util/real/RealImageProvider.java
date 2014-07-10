77ace8094ac4b358bfa1be33039f0aaea875b2b5
Prajit Kumar Das <prajitdas@gmail.com>
2014-07-09 17:29:46 -0700

    real provider bug fixed

--- src/com/prajitdas/sprivacy/contentprovider/util/real/RealImageProvider.java
+++ src/com/prajitdas/sprivacy/contentprovider/util/real/RealImageProvider.java
@@ -1,13 +1,16 @@
 package com.prajitdas.sprivacy.contentprovider.util.real;
 
 import com.prajitdas.sprivacy.ProviderApplication;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore.Images;
 import android.provider.MediaStore.Images.Media;
 
-public class RealImageProvider {
+public class RealImageProvider extends ContentProvider {
 	private static final String PROVIDER_NAME = "com.prajitdas.sprivacy.contentprovider.util.real";
 	private static final String URL = "content://" + PROVIDER_NAME + "/images";
 	public static final Uri CONTENT_URI = Uri.parse(URL);
@@ -18,6 +21,8 @@ public class RealImageProvider {
 		uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 		uriMatcher.addURI(PROVIDER_NAME, "images", IMAGES);
 	}
+	
+	@Override
 	public Cursor query(Uri uri, String[] projection, String selection,
 			String[] selectionArgs, String sortOrder) {
 		return Media.query(	ProviderApplication.getSingleton().getContentResolver(),
@@ -38,4 +43,30 @@ public class RealImageProvider {
 //	    String[] selectionArgs = null;
 //	    String sort = ImageColumns._ID + " DESC LIMIT 1";
     }
+	@Override
+	public boolean onCreate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public String getType(Uri uri) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
