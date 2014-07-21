710f1bfbb791fd5bcae1ff114edb2f7cb5caba86
Prajit Kumar Das <prajitdas@gmail.com>
2014-07-21 14:23:20 -0700

    access levels included in policy db helper implementation remains

--- src/com/prajitdas/sprivacy/policymanager/PolicyDBHelper.java
+++ src/com/prajitdas/sprivacy/policymanager/PolicyDBHelper.java
@@ -26,6 +26,7 @@ public class PolicyDBHelper extends SQLiteOpenHelper {
 	private final static String POLAPPID = "appid";
 	private final static String POLRESID = "resid";
 	private final static String POLICY = "policy";
+	private final static String ACCESSLVL = "accesslvl";
 	
 	// database declarations
 	private final static String DATABASE_NAME = "PrivacyPolicies";
@@ -42,12 +43,22 @@ public class PolicyDBHelper extends SQLiteOpenHelper {
 	private final static String CREATE_RESOURCE_TABLE =  " CREATE TABLE " + RESOURCE_TABLE_NAME + " (" + 
 			RESID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
 			RESNAME + " TEXT NOT NULL UNIQUE);";
-
+	/**
+	 *  A value of 1 in the policy column refers to a policy of access granted
+	 *  A value of 0 in the policy column refers to a policy of access denied
+	 *  
+	 *  A value of 0 in the accesslvl column refers to access level of "give all data"
+	 *  A value of 1 in the accesslvl column refers to access level of "give limited data"
+	 *  A value of 2 in the accesslvl column refers to access level of "give fake data"
+	 *  A value of 3 in the accesslvl column refers to access level of "give no data"
+	 */
 	private final static String CREATE_POLICY_TABLE =  " CREATE TABLE " + POLICY_TABLE_NAME + " (" +
 			POLID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
 			POLAPPID + " INTEGER NOT NULL REFERENCES applications(id) ON DELETE CASCADE, " +
 			POLRESID + " INTEGER NOT NULL REFERENCES resources(id) ON DELETE CASCADE, " +
-			POLICY + " INTEGER DEFAULT 0);";
+			POLICY + " INTEGER DEFAULT 1" +
+			ACCESSLVL + "INTEGER DEFAULT 0);";
+	
 	
 	private static DefaultDataLoader defaultDataLoader;
 	
