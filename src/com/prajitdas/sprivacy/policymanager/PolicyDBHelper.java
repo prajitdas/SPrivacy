c9736271eab946a2314d68118b6c3d6372f4c31e
Prajit Kumar Das <prajitdas@gmail.com>
2014-07-22 12:06:00 -0700

    apks added

--- src/com/prajitdas/sprivacy/policymanager/PolicyDBHelper.java
+++ src/com/prajitdas/sprivacy/policymanager/PolicyDBHelper.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import com.prajitdas.sprivacy.SPrivacyApplication;
 import com.prajitdas.sprivacy.policymanager.util.AppInfo;
 import com.prajitdas.sprivacy.policymanager.util.DefaultDataLoader;
 import com.prajitdas.sprivacy.policymanager.util.PolicyRule;
@@ -81,10 +83,16 @@ public class PolicyDBHelper extends SQLiteOpenHelper {
 	 * method to insert into application table the application
 	 * @param value the name of the application
 	 */
-	public void addApplication(SQLiteDatabase db, AppInfo anAppInfo) {
+	public int addApplication(SQLiteDatabase db, AppInfo anAppInfo) {
 		ContentValues values = new ContentValues();
 		values.put(APPNAME, anAppInfo.getName());
-		db.insert(APPLICATION_TABLE_NAME, null, values);
+		try{
+			db.insert(APPLICATION_TABLE_NAME, null, values);
+		} catch (SQLException e) {
+            Log.e(SPrivacyApplication.getDebugTag(), "Error inserting " + values, e);
+            return -1;
+		}
+		return 1;
 	}
 	
 	/**
@@ -97,7 +105,7 @@ public class PolicyDBHelper extends SQLiteOpenHelper {
 	 * @param resId the resource id
 	 * @param policy the policy value
 	 */
-	public void addPolicy(SQLiteDatabase db, PolicyRule aPolicyRule) {
+	public int addPolicy(SQLiteDatabase db, PolicyRule aPolicyRule) {
 		ContentValues values = new ContentValues();
 		values.put(POLAPPID, aPolicyRule.getAppId());
 		values.put(POLRESID, aPolicyRule.getResId());
@@ -105,17 +113,29 @@ public class PolicyDBHelper extends SQLiteOpenHelper {
 			values.put(POLICY, 1);
 		else
 			values.put(POLICY, 0);
-		db.insert(POLICY_TABLE_NAME, null, values);
+		try {
+			db.insert(POLICY_TABLE_NAME, null, values);
+		} catch (SQLException e) {
+	        Log.e(SPrivacyApplication.getDebugTag(), "Error inserting " + values, e);
+	        return -1;
+		}
+		return 1;
 	}
 
 	/**
 	 * method to insert into resource table the resource
 	 * @param value the name of the resource
 	 */
-	public void addResource(SQLiteDatabase db, Resource aResource) {
+	public int addResource(SQLiteDatabase db, Resource aResource) {
 		ContentValues values = new ContentValues();
 		values.put(RESNAME, aResource.getName());
-		db.insert(RESOURCE_TABLE_NAME, null, values);
+		try {
+			db.insert(RESOURCE_TABLE_NAME, null, values);
+		} catch (SQLException e) {
+	        Log.e(SPrivacyApplication.getDebugTag(), "Error inserting " + values, e);
+	        return -1;
+		}
+		return 1;
 	}
 	
 	/**
@@ -149,126 +169,6 @@ public class PolicyDBHelper extends SQLiteOpenHelper {
 	public void deleteResource(SQLiteDatabase db, Resource aResource) {
 		db.delete(RESOURCE_TABLE_NAME, RESID + " = ?",
 				new String[] { String.valueOf(aResource.getId()) });
-	}
-	
-	/**
-	 * method to load the default set of policies into the database
-	 * @param db reference to the db instance
-	 */
-	private void loadDefaultPoliciesIntoDB(SQLiteDatabase db) {
-		for(AppInfo anAppInfo : defaultDataLoader.getApplications())
-			addApplication(db, anAppInfo);
-		for(Resource aResource : defaultDataLoader.getResources())
-			addResource(db, aResource);
-		for(PolicyRule aPolicyRule : defaultDataLoader.getPolicies())
-			addPolicy(db, aPolicyRule);
-	}
-	
-	/**
-	 * table creation happens in onCreate this method also loads the default policies
-	 */
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		db.execSQL(CREATE_APPLICATION_TABLE);
-		db.execSQL(CREATE_RESOURCE_TABLE);
-		db.execSQL(CREATE_POLICY_TABLE);
-		loadDefaultPoliciesIntoDB(db);
-	}
-	
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		Log.w(PolicyDBHelper.class.getName(), 
-				"Upgrading database from version " + oldVersion + " to "
-						+ newVersion + ". Old data will be destroyed");
-		db.execSQL("DROP TABLE IF EXISTS " +  APPLICATION_TABLE_NAME);
-		db.execSQL("DROP TABLE IF EXISTS " +  RESOURCE_TABLE_NAME);
-		db.execSQL("DROP TABLE IF EXISTS " +  POLICY_TABLE_NAME);
-		onCreate(db);
-	}
-	
-	/**
-	 * method to update single application
-	 * @param appName
-	 */
-	public int updateApplication(SQLiteDatabase db, AppInfo anAppInfo) {
-		ContentValues values = new ContentValues();
-		values.put(APPNAME, anAppInfo.getName());
-		return db.update(APPLICATION_TABLE_NAME, values, APPID + " = ?", 
-				new String[] { String.valueOf(anAppInfo.getId()) });
-	}
-
-	/**
-	 * method to update single policy
-	 * @param aPolicyRule update the policy value
-	 */
-	public int updatePolicyRule(SQLiteDatabase db, PolicyRule aPolicyRule) {
-		ContentValues values = new ContentValues();
-		values.put(POLAPPID, aPolicyRule.getAppId());
-		values.put(POLRESID, aPolicyRule.getResId());
-		if(aPolicyRule.isPolicyRule())
-			values.put(POLICY, 1);
-		else
-			values.put(POLICY, 0);
-		return db.update(POLICY_TABLE_NAME, values, POLID + " = ?", 
-				new String[] { String.valueOf(aPolicyRule.getId()) });
-	}
-	
-	/**
-	 * method to update single resource
-	 * @param resName
-	 */
-	public int updateResource(SQLiteDatabase db, Resource aResource) {
-		ContentValues values = new ContentValues();
-		values.put(RESNAME, aResource.getName());
-		return db.update(RESOURCE_TABLE_NAME, values, RESID + " = ?", 
-				new String[] { String.valueOf(aResource.getId()) });
-	}
-
-	/**
-	 * Getting all policies
-	 * @return returns a list of policies
-	 */
-	public ArrayList<PolicyRule> getAllPolicies(SQLiteDatabase db) {
-		ArrayList<PolicyRule> policyRules = new ArrayList<PolicyRule>();
-		// Select All Query
-		String selectQuery = "SELECT "+
-					POLICY_TABLE_NAME + "." + POLID + "," +
-					APPLICATION_TABLE_NAME + "." + APPID + "," +
-					APPLICATION_TABLE_NAME + "." + APPNAME + "," +
-					RESOURCE_TABLE_NAME + "." + RESID + "," +
-					RESOURCE_TABLE_NAME + "." + RESNAME + "," +
-					POLICY_TABLE_NAME + "." + POLICY +
-					" FROM " + 
-					POLICY_TABLE_NAME +
-					" LEFT JOIN " + APPLICATION_TABLE_NAME + 
-					" ON " + POLICY_TABLE_NAME + "." + POLAPPID + 
-					" = " +  APPLICATION_TABLE_NAME + "." + APPID +
-					" LEFT JOIN " + RESOURCE_TABLE_NAME + 
-					" ON " + POLICY_TABLE_NAME + "." + POLRESID + 
-					" = " +  RESOURCE_TABLE_NAME + "." + RESID + ";";
-
-		Cursor cursor = db.rawQuery(selectQuery, null);
-
-		// looping through all rows and adding to list
-		if (cursor.moveToFirst()) {
-			do {
-				PolicyRule policyRule = new PolicyRule();
-				policyRule.setId(Integer.parseInt(cursor.getString(0)));
-				policyRule.setAppId(Integer.parseInt(cursor.getString(1)));
-				policyRule.setAppName(cursor.getString(2));
-				policyRule.setResId(Integer.parseInt(cursor.getString(3)));
-				policyRule.setResName(cursor.getString(4));
-				if(Integer.parseInt(cursor.getString(5)) == 1)
-					policyRule.setPolicyRule(true);
-				else
-					policyRule.setPolicyRule(false);
-				// Adding policies to list
-				policyRules.add(policyRule);
-			} while (cursor.moveToNext());
-		}
-
-		// return policy rules list
-		return policyRules;
 	}
 	
 	/**
@@ -357,7 +257,132 @@ public class PolicyDBHelper extends SQLiteOpenHelper {
 		return policyRule;
 	}
 	
+	/**
+	 * Getting all policies
+	 * @return returns a list of policies
+	 */
+	public ArrayList<PolicyRule> getAllPolicies(SQLiteDatabase db) {
+		ArrayList<PolicyRule> policyRules = new ArrayList<PolicyRule>();
+		// Select All Query
+		String selectQuery = "SELECT "+
+					POLICY_TABLE_NAME + "." + POLID + "," +
+					APPLICATION_TABLE_NAME + "." + APPID + "," +
+					APPLICATION_TABLE_NAME + "." + APPNAME + "," +
+					RESOURCE_TABLE_NAME + "." + RESID + "," +
+					RESOURCE_TABLE_NAME + "." + RESNAME + "," +
+					POLICY_TABLE_NAME + "." + POLICY +
+					" FROM " + 
+					POLICY_TABLE_NAME +
+					" LEFT JOIN " + APPLICATION_TABLE_NAME + 
+					" ON " + POLICY_TABLE_NAME + "." + POLAPPID + 
+					" = " +  APPLICATION_TABLE_NAME + "." + APPID +
+					" LEFT JOIN " + RESOURCE_TABLE_NAME + 
+					" ON " + POLICY_TABLE_NAME + "." + POLRESID + 
+					" = " +  RESOURCE_TABLE_NAME + "." + RESID + ";";
+
+		Cursor cursor = db.rawQuery(selectQuery, null);
+
+		// looping through all rows and adding to list
+		if (cursor.moveToFirst()) {
+			do {
+				PolicyRule policyRule = new PolicyRule();
+				policyRule.setId(Integer.parseInt(cursor.getString(0)));
+				policyRule.setAppId(Integer.parseInt(cursor.getString(1)));
+				policyRule.setAppName(cursor.getString(2));
+				policyRule.setResId(Integer.parseInt(cursor.getString(3)));
+				policyRule.setResName(cursor.getString(4));
+				if(Integer.parseInt(cursor.getString(5)) == 1)
+					policyRule.setPolicyRule(true);
+				else
+					policyRule.setPolicyRule(false);
+				// Adding policies to list
+				policyRules.add(policyRule);
+			} while (cursor.moveToNext());
+		}
+
+		// return policy rules list
+		return policyRules;
+	}
+	
 	public String getDatabaseName() {
 		return DATABASE_NAME;
+	}
+
+	/**
+	 * method to load the default set of policies into the database
+	 * @param db reference to the db instance
+	 */
+	private void loadDefaultPoliciesIntoDB(SQLiteDatabase db) {
+		//loads the applications
+		for(AppInfo anAppInfo : defaultDataLoader.getApplications())
+			addApplication(db, anAppInfo);
+		//loads the resources or providers
+		for(Resource aResource : defaultDataLoader.getResources())
+			addResource(db, aResource);
+		//loads the policies, this is the interesting part and can be used to load
+		//a default set of policies from an xml resource or a web service
+		for(PolicyRule aPolicyRule : defaultDataLoader.getPolicies())
+			addPolicy(db, aPolicyRule);
+	}
+	
+	/**
+	 * table creation happens in onCreate this method also loads the default policies
+	 */
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_APPLICATION_TABLE);
+		db.execSQL(CREATE_RESOURCE_TABLE);
+		db.execSQL(CREATE_POLICY_TABLE);
+		//The following method loads the database with the default data on creation of the database
+		loadDefaultPoliciesIntoDB(db);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.w(PolicyDBHelper.class.getName(), 
+				"Upgrading database from version " + oldVersion + " to "
+						+ newVersion + ". Old data will be destroyed");
+		db.execSQL("DROP TABLE IF EXISTS " +  APPLICATION_TABLE_NAME);
+		db.execSQL("DROP TABLE IF EXISTS " +  RESOURCE_TABLE_NAME);
+		db.execSQL("DROP TABLE IF EXISTS " +  POLICY_TABLE_NAME);
+		onCreate(db);
+	}
+	
+	/**
+	 * method to update single application
+	 * @param appName
+	 */
+	public int updateApplication(SQLiteDatabase db, AppInfo anAppInfo) {
+		ContentValues values = new ContentValues();
+		values.put(APPNAME, anAppInfo.getName());
+		return db.update(APPLICATION_TABLE_NAME, values, APPID + " = ?", 
+				new String[] { String.valueOf(anAppInfo.getId()) });
+	}
+	
+	/**
+	 * method to update single policy
+	 * @param aPolicyRule update the policy value
+	 */
+	public int updatePolicyRule(SQLiteDatabase db, PolicyRule aPolicyRule) {
+		ContentValues values = new ContentValues();
+		values.put(POLAPPID, aPolicyRule.getAppId());
+		values.put(POLRESID, aPolicyRule.getResId());
+		if(aPolicyRule.isPolicyRule())
+			values.put(POLICY, 1);
+		else
+			values.put(POLICY, 0);
+		return db.update(POLICY_TABLE_NAME, values, POLID + " = ?", 
+				new String[] { String.valueOf(aPolicyRule.getId()) });
+	}
+	
+	/**
+	 * method to update single resource
+	 * @param resName
+	 */
+	public int updateResource(SQLiteDatabase db, Resource aResource) {
+		ContentValues values = new ContentValues();
+		values.put(RESNAME, aResource.getName());
+		return db.update(RESOURCE_TABLE_NAME, values, RESID + " = ?", 
+				new String[] { String.valueOf(aResource.getId()) });
 	}
 }
