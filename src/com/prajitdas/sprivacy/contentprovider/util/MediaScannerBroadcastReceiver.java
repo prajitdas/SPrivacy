367a6eb5b22ba10b4432c9a6abc04f29d42cbcd0
Prajit Kumar Das <prajitdas@gmail.com>
2014-08-06 17:07:04 -0700

    a Brodacast receiver has been used its not working properly right now. Some hardcoding was done. Will have to remove that later

---
+++ src/com/prajitdas/sprivacy/contentprovider/util/MediaScannerBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package com.prajitdas.sprivacy.contentprovider.util;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MediaScannerBroadcastReceiver extends BroadcastReceiver {
+	public static boolean mMediaScanning = true;
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if(intent.getAction().equals(Intent.ACTION_MEDIA_SCANNER_STARTED)){
+			mMediaScanning = true;
+		}
+		if(intent.getAction().equals(Intent.ACTION_MEDIA_SCANNER_FINISHED)){
+			mMediaScanning = false;
+		}
+	}
+}
