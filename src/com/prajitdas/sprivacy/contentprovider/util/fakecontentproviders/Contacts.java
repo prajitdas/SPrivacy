9c57bb873ae71e0f68408874aafb8769d41a497b
Prajit Kumar Das <prajitdas@gmail.com>
2014-08-14 14:54:59 -0700

    debug code added

--- src/com/prajitdas/sprivacy/contentprovider/util/fakecontentproviders/Contacts.java
+++ src/com/prajitdas/sprivacy/contentprovider/util/fakecontentproviders/Contacts.java
@@ -34,95 +34,6 @@ public class Contacts extends ContentProvider {
 	 *  fields for the database
 	 */
 	static final String CONTACTS_TABLE_ID = "_id";
-    /**
-     * The display name for the contact.
-     * <P>Type: TEXT</P>
-     */
-    static final String DISPLAY_NAME = "display_name";
-    /**
-     * Reference to the row in the data table holding the photo.  A photo can
-     * be referred to either by ID (this field) or by URI (see {@link #PHOTO_THUMBNAIL_URI}
-     * and {@link #PHOTO_URI}).
-     * If PHOTO_ID is null, consult {@link #PHOTO_URI} or {@link #PHOTO_THUMBNAIL_URI},
-     * which is a more generic mechanism for referencing the contact photo, especially for
-     * contacts returned by non-local directories (see {@link Directory}).
-     *
-     * <P>Type: INTEGER REFERENCES data(_id)</P>
-     */
-    public static final String PHOTO_ID = "photo_id";
-    /**
-     * Photo file ID of the full-size photo.  If present, this will be used to populate
-     * {@link #PHOTO_URI}.  The ID can also be used with
-     * {@link FakeContract.DisplayPhoto#CONTENT_URI} to create a URI to the photo.
-     * If this is present, {@link #PHOTO_ID} is also guaranteed to be populated.
-     *
-     * <P>Type: INTEGER</P>
-     */
-    static final String PHOTO_FILE_ID = "photo_file_id";
-    /**
-     * A URI that can be used to retrieve the contact's full-size photo.
-     * If PHOTO_FILE_ID is not null, this will be populated with a URI based off
-     * {@link FakeContract.DisplayPhoto#CONTENT_URI}.  Otherwise, this will
-     * be populated with the same value as {@link #PHOTO_THUMBNAIL_URI}.
-     * A photo can be referred to either by a URI (this field) or by ID
-     * (see {@link #PHOTO_ID}). If either PHOTO_FILE_ID or PHOTO_ID is not null,
-     * PHOTO_URI and PHOTO_THUMBNAIL_URI shall not be null (but not necessarily
-     * vice versa).  Thus using PHOTO_URI is a more robust method of retrieving
-     * contact photos.
-     *
-     * <P>Type: TEXT</P>
-     */
-    static final String PHOTO_URI = "photo_uri";
-    /**
-     * A URI that can be used to retrieve a thumbnail of the contact's photo.
-     * A photo can be referred to either by a URI (this field or {@link #PHOTO_URI})
-     * or by ID (see {@link #PHOTO_ID}). If PHOTO_ID is not null, PHOTO_URI and
-     * PHOTO_THUMBNAIL_URI shall not be null (but not necessarily vice versa).
-     * If the content provider does not differentiate between full-size photos
-     * and thumbnail photos, PHOTO_THUMBNAIL_URI and {@link #PHOTO_URI} can contain
-     * the same value, but either both shall be null or both not null.
-     *
-     * <P>Type: TEXT</P>
-     */
-    static final String PHOTO_THUMBNAIL_URI = "photo_thumb_uri";
-    /**
-     * Flag that reflects the {@link Groups#GROUP_VISIBLE} state of any
-     * {@link CommonDataKinds.Gr			// maps all database column names
-oupMembership} for this contact.
-     */
-    static final String IN_VISIBLE_GROUP = "in_visible_group";
-    /**
-     * Flag that reflects whether this contact represents the user's
-     * personal profile entry.
-     */
-    static final String IS_USER_PROFILE = "is_user_profile";
-    /**
-     * An indicator of whether this contact has at least one phone number. "1" if there is
-     * at least one phone number, "0" otherwise.
-     * <P>Type: INTEGER</P>
-     */
-    static final String HAS_PHONE_NUMBER = "has_phone_number";
-    /**
-     * An opaque value that contains hints on how to find the contact if
-     * its row id changed as a result of a sync or aggregation.
-     */
-    static final String LOOKUP_KEY = "lookup";
-    /**
-     * Timestamp (milliseconds since epoch) of when this contact was last updated.  This
-     * includes updates to all data associated with this contact including raw contacts.  Any
-     * modification (including deletes and inserts) of underlying contact data are also
-     * reflected in this timestamp.
-     */
-    static final String CONTACT_LAST_UPDATED_TIMESTAMP =
-            "contact_last_updated_timestamp";
-    /**
-     * Sort key that takes into account locale-based traditions for sorting
-     * names in address books.  The default
-     * sort key is {@link #DISPLAY_NAME_PRIMARY}.  For Chinese names
-     * the sort key is the name's Pinyin spelling, and for Japanese names
-     * it is the Hiragana version of the phonetic name.
-     */
-    public static final String SORT_KEY_PRIMARY = "sort_key";
     
     /**
 	 * integer values used in content URI
@@ -159,17 +70,17 @@ oupMembership} for this contact.
 	static final int DATABASE_VERSION = 1;
 	static final String CREATE_DB_TABLE = " CREATE TABLE " + TABLE_NAME + " (" + 
 			CONTACTS_TABLE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
-			DISPLAY_NAME + " TEXT NOT NULL, " +
-			SORT_KEY_PRIMARY + " TEXT NOT NULL, " +
-			PHOTO_ID + " INTEGER, " +
-			PHOTO_FILE_ID + " INTEGER, " +
-			PHOTO_URI + " TEXT, " +
-			PHOTO_THUMBNAIL_URI + " TEXT, " +
-			IN_VISIBLE_GROUP + " INTEGER, " +
-			IS_USER_PROFILE + " INTEGER, " +
-			HAS_PHONE_NUMBER + " INTEGER, " +
-			LOOKUP_KEY + " TEXT, " +
-			CONTACT_LAST_UPDATED_TIMESTAMP + " INTEGER, "+
+			ContactsContract.Data.DISPLAY_NAME + " TEXT NOT NULL, " +
+			ContactsContract.Data.SORT_KEY_PRIMARY + " TEXT NOT NULL, " +
+			ContactsContract.Data.PHOTO_ID + " INTEGER, " +
+			ContactsContract.Data.PHOTO_FILE_ID + " INTEGER, " +
+			ContactsContract.Data.PHOTO_URI + " TEXT, " +
+			ContactsContract.Data.PHOTO_THUMBNAIL_URI + " TEXT, " +
+			ContactsContract.Data.IN_VISIBLE_GROUP + " INTEGER, " +
+			ContactsContract.Data.IS_USER_PROFILE + " INTEGER, " +
+			ContactsContract.Data.HAS_PHONE_NUMBER + " INTEGER, " +
+			ContactsContract.Data.LOOKUP_KEY + " TEXT, " +
+			ContactsContract.Data.CONTACT_LAST_UPDATED_TIMESTAMP + " INTEGER, "+
 			ContactAddressQuery.FORMATTED_ADDRESS + " TEXT, "+
 			ContactAddressQuery.ADDRESS_TYPE + " TEXT, "+
 			ContactAddressQuery.ADDRESS_LABEL + " TEXT, " +
@@ -216,21 +127,21 @@ oupMembership} for this contact.
 		private int loaDefaultData(SQLiteDatabase db) {
 			//Data Set 1
 			ContentValues values1 = new ContentValues();
-			values1.put(DISPLAY_NAME,"John Doe");
-			values1.put(SORT_KEY_PRIMARY,"John Doe");
-			values1.put(PHOTO_ID,"");
-			values1.put(PHOTO_FILE_ID,"");
-			values1.put(PHOTO_URI,"");
-			values1.put(PHOTO_THUMBNAIL_URI,"");
-			values1.put(IN_VISIBLE_GROUP,"1");
-			values1.put(IS_USER_PROFILE,"");
-			values1.put(HAS_PHONE_NUMBER,"4567890123");
-			values1.put(LOOKUP_KEY,"johndoe");
-			values1.put(CONTACT_LAST_UPDATED_TIMESTAMP,"1407544837");
+			values1.put(ContactsContract.Data.DISPLAY_NAME,"John Doe");
+			values1.put(ContactsContract.Data.SORT_KEY_PRIMARY,"John Doe");
+			values1.put(ContactsContract.Data.PHOTO_ID,"");
+			values1.put(ContactsContract.Data.PHOTO_FILE_ID,"");
+			values1.put(ContactsContract.Data.PHOTO_URI,"");
+			values1.put(ContactsContract.Data.PHOTO_THUMBNAIL_URI,"");
+			values1.put(ContactsContract.Data.IN_VISIBLE_GROUP,"1");
+			values1.put(ContactsContract.Data.IS_USER_PROFILE,"");
+			values1.put(ContactsContract.Data.HAS_PHONE_NUMBER,"4567890123");
+			values1.put(ContactsContract.Data.LOOKUP_KEY,"johndoe");
+			values1.put(ContactsContract.Data.CONTACT_LAST_UPDATED_TIMESTAMP, System.currentTimeMillis());
 			values1.put(ContactAddressQuery.FORMATTED_ADDRESS,"1 Mordor Lane, Mordor, Middlearth");
 			values1.put(ContactAddressQuery.ADDRESS_TYPE,"Home");
 			values1.put(ContactAddressQuery.ADDRESS_LABEL,"Home");
-			values1.put(ContactsContract.Data.MIMETYPE,"vnd.android.cursor.item/postal-address_v2");
+			values1.put(ContactsContract.Data.MIMETYPE,"vnd.android.cursor.item/name");
 			values1.put(ContactsContract.Data.DATA_VERSION,"1");
 			values1.put(ContactsContract.Data.IS_PRIMARY,"0");
 			values1.put(ContactsContract.Data.IS_SUPER_PRIMARY,"0");
