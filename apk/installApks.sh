535346256f6b206265975d1636e9f03e61758b60
Prajit Kumar Das <prajitdas@gmail.com>
2014-08-20 16:30:31 -0700

    script updated

--- apk/installApks.sh
+++ apk/installApks.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+adb install -r Focal_fr.xplod.focal_1.0-4aeccf19_9.apk
 adb install -r SIFContentProvider_de.vogella.android.todos_1.0_1.apk
 adb install -r QuickPic_com.alensw.PicFolder_3.7.0_142.apk
 adb install -r exDialer_com.modoohut.dialer_176_176.apk
