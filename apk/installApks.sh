044e23aab952049f4f41d91332fb38d518527939
Prajit Kumar Das <prajitdas@gmail.com>
2014-08-20 15:43:52 -0700

    apk updated

--- apk/installApks.sh
+++ apk/installApks.sh
@@ -3,3 +3,4 @@ adb install -r SIFContentProvider_de.vogella.android.todos_1.0_1.apk
 adb install -r QuickPic_com.alensw.PicFolder_3.7.0_142.apk
 adb install -r exDialer_com.modoohut.dialer_176_176.apk
 adb install -r Android\ Device\ ID_com.evozi.deviceid_1.2.1_6.apk
+adb install -r SPrivacy.apk
